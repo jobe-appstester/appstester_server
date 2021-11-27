71364f726c97ce56a37998cf1b6739ed7933e740
Ilya Gavrilov <42314637+devgavrilov@users.noreply.github.com>
2021-11-27 16:17:46 +0300

    Device monitor & Initialization in constructor.

--- AppsTester.Checker.Android/Adb/AdbClientProvider.cs
+++ AppsTester.Checker.Android/Adb/AdbClientProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -5,42 +6,47 @@ using SharpAdbClient;
 
 namespace AppsTester.Checker.Android.Adb
 {
-    internal interface IAdbClientProvider
+    internal interface IAdbClientProvider : IDisposable
     {
         IAdbClient GetAdbClient();
     }
 
     internal class AdbClientProvider : IAdbClientProvider
     {
-        private readonly IConfiguration _configuration;
-        private readonly ILogger<AdbClientProvider> _logger;
-
-        private IAdbClient _adbClient;
+        private readonly IAdbClient _adbClient;
+        private readonly IDeviceMonitor _deviceMonitor;
 
         public AdbClientProvider(IConfiguration configuration, ILogger<AdbClientProvider> logger)
         {
-            _configuration = configuration;
-            _logger = logger;
+            var dnsEndPoint = new DnsEndPoint(configuration["Adb:Host"], port: 5037);
+            _adbClient = new AdbClient(dnsEndPoint, adbSocketFactory: Factories.AdbSocketFactory);
+
+            logger.LogInformation($"Connecting to ADB server at {configuration["Adb:Host"]}:5037.");
+
+            var version = _adbClient.GetAdbVersion();
+
+            logger.LogInformation(
+                $"Successfully connected to ADB server at {configuration["Adb:Host"]}:5037. Version is {version}.");
+
+            _deviceMonitor = new DeviceMonitor(new AdbSocket(dnsEndPoint));
+
+            _deviceMonitor.DeviceConnected += (_, args) =>
+                logger.LogInformation("Connected device with serial {Serial}", args.Device.Serial);
+
+            _deviceMonitor.DeviceDisconnected += (_, args) =>
+                logger.LogInformation("Disconnected device with serial {Serial}", args.Device.Serial);
+
+            _deviceMonitor.DeviceChanged += (_, args) =>
+                logger.LogInformation("Changed device with serial {Serial}", args.Device.Serial);
+
+            _deviceMonitor.Start();
         }
 
-        public IAdbClient GetAdbClient()
+        public IAdbClient GetAdbClient() => _adbClient;
+
+        public void Dispose()
         {
-            if (_adbClient != null)
-                return _adbClient;
-
-            var dnsEndPoint = new DnsEndPoint(_configuration["Adb:Host"], port: 5037);
-            var adbClient = new AdbClient(dnsEndPoint, adbSocketFactory: Factories.AdbSocketFactory);
-
-            _logger.LogInformation($"Connecting to ADB server at {_configuration["Adb:Host"]}:5037.");
-
-            var version = adbClient.GetAdbVersion();
-            
-            _logger.LogInformation(
-                $"Successfully connected to ADB server at {_configuration["Adb:Host"]}:5037. Version is {version}.");
-
-            _adbClient = adbClient;
-
-            return _adbClient;
+            _deviceMonitor.Dispose();
         }
     }
 }
