682bee2b9af0ba9c87ad381091cd8f01350c722f
Ilya Gavrilov <42314637+devgavrilov@users.noreply.github.com>
2021-11-27 23:50:33 +0300

    Explicit JSON property names.

--- AppsTester.Controller/Submissions/Submission.cs
+++ AppsTester.Controller/Submissions/Submission.cs
@@ -1,12 +1,20 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace AppsTester.Controller.Submissions
 {
     public sealed class Submission
     {
+        [JsonProperty("attempt_id")]
         public int AttemptId { get; set; }
+
+        [JsonProperty("checker_system_name")]
         public string CheckerSystemName { get; set; }
+
+        [JsonProperty("parameters")]
         public Dictionary<string, object> Parameters { get; set; }
+
+        [JsonProperty("files")]
         public Dictionary<string, string> Files { get; set; }
     }
 }
