731084db1eed710737dd8e1120486057d5851994
Ilya Gavrilov <42314637+devgavrilov@users.noreply.github.com>
2022-02-16 15:01:07 +0300

    Retry any exceptions.

--- AppsTester.Controller/Submissions/SubmissionsInfoSynchronizer.cs
+++ AppsTester.Controller/Submissions/SubmissionsInfoSynchronizer.cs
@@ -34,81 +34,91 @@ namespace AppsTester.Controller.Submissions
         {
             while (true)
             {
-                var attemptIds = await _moodleCommunicator.GetFunctionResultAsync<int[]>(
-                    functionName: "local_qtype_get_submissions_to_check", cancellationToken: stoppingToken);
-
-                if (attemptIds == null || !attemptIds.Any())
+                try
                 {
-                    await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
-                    continue;
-                }
+                    var attemptIds = await _moodleCommunicator.GetFunctionResultAsync<int[]>(
+                        functionName: "local_qtype_get_submissions_to_check", cancellationToken: stoppingToken);
 
-                using var serviceScope = _serviceScopeFactory.CreateScope();
-                await using var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-                foreach (var attemptId in attemptIds)
-                {
-                    if (dbContext.SubmissionChecks.Any(sc => sc.AttemptId == attemptId))
+                    if (attemptIds == null || !attemptIds.Any())
                     {
                         await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
                         continue;
                     }
 
-                    var submission = await _moodleCommunicator.GetFunctionResultAsync<Submission>(
-                        functionName: "local_qtype_get_submission",
-                        functionParams: new Dictionary<string, object> { ["id"] = attemptId },
-                        cancellationToken: stoppingToken);
+                    using var serviceScope = _serviceScopeFactory.CreateScope();
+                    await using var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-                    var fileCache = serviceScope.ServiceProvider.GetRequiredService<FileCache>();
-
-                    var missingFiles = submission
-                        .Files
-                        .Where(pair => pair.Key.EndsWith("_hash"))
-                        .Where(pair => !fileCache.IsKeyExists(pair.Value))
-                        .ToList();
-
-                    if (missingFiles.Any())
+                    foreach (var attemptId in attemptIds)
                     {
-                        submission = await _moodleCommunicator.GetFunctionResultAsync<Submission>(
+                        if (dbContext.SubmissionChecks.Any(sc => sc.AttemptId == attemptId))
+                        {
+                            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+                            continue;
+                        }
+
+                        var submission = await _moodleCommunicator.GetFunctionResultAsync<Submission>(
                             functionName: "local_qtype_get_submission",
-                            functionParams: new Dictionary<string,object>
-                            {
-                                ["id"] = attemptId,
-                                ["included_file_hashes"] = string.Join(",", missingFiles.Select(mf => mf.Value))
-                            },
+                            functionParams: new Dictionary<string, object> { ["id"] = attemptId },
                             cancellationToken: stoppingToken);
 
-                        foreach (var (fileName, fileHash) in missingFiles)
-                            fileCache.Write(fileHash,
-                                Convert.FromBase64String(submission.Files[fileName.Substring(0, fileName.Length - 5)]));
+                        var fileCache = serviceScope.ServiceProvider.GetRequiredService<FileCache>();
+
+                        var missingFiles = submission
+                            .Files
+                            .Where(pair => pair.Key.EndsWith("_hash"))
+                            .Where(pair => !fileCache.IsKeyExists(pair.Value))
+                            .ToList();
+
+                        if (missingFiles.Any())
+                        {
+                            submission = await _moodleCommunicator.GetFunctionResultAsync<Submission>(
+                                functionName: "local_qtype_get_submission",
+                                functionParams: new Dictionary<string, object>
+                                {
+                                    ["id"] = attemptId,
+                                    ["included_file_hashes"] = string.Join(",", missingFiles.Select(mf => mf.Value))
+                                },
+                                cancellationToken: stoppingToken);
+
+                            foreach (var (fileName, fileHash) in missingFiles)
+                                fileCache.Write(fileHash,
+                                    Convert.FromBase64String(
+                                        submission.Files[fileName.Substring(0, fileName.Length - 5)]));
+                        }
+
+                        var submissionId = Guid.NewGuid();
+                        var submissionCheckRequest = new SubmissionCheckRequestEvent
+                        {
+                            SubmissionId = submissionId,
+                            Files = submission.Files.Where(f => f.Key.EndsWith("_hash"))
+                                .ToDictionary(pair => pair.Key.Substring(0, pair.Key.Length - 5), pair => pair.Value),
+                            PlainParameters = submission.Parameters
+                        };
+                        var submissionCheck = new SubmissionCheck
+                        {
+                            Id = submissionId,
+                            AttemptId = attemptId,
+                            SendingDateTimeUtc = DateTime.UtcNow,
+                            SerializedRequest = JsonConvert.SerializeObject(submissionCheckRequest)
+                        };
+                        dbContext.SubmissionChecks.Add(submissionCheck);
+
+                        var rabbitConnection = _rabbitBusProvider.GetRabbitBus();
+                        await rabbitConnection.PubSub.PublishAsync(submissionCheckRequest,
+                            topic: submission.CheckerSystemName,
+                            cancellationToken: stoppingToken);
+
+                        await dbContext.SaveChangesAsync(stoppingToken);
                     }
-
-                    var submissionId = Guid.NewGuid();
-                    var submissionCheckRequest = new SubmissionCheckRequestEvent
-                    {
-                        SubmissionId = submissionId,
-                        Files = submission.Files.Where(f => f.Key.EndsWith("_hash"))
-                            .ToDictionary(pair => pair.Key.Substring(0, pair.Key.Length - 5), pair => pair.Value),
-                        PlainParameters = submission.Parameters
-                    };
-                    var submissionCheck = new SubmissionCheck
-                    {
-                        Id = submissionId,
-                        AttemptId = attemptId,
-                        SendingDateTimeUtc = DateTime.UtcNow,
-                        SerializedRequest = JsonConvert.SerializeObject(submissionCheckRequest)
-                    };
-                    dbContext.SubmissionChecks.Add(submissionCheck);
-
-                    var rabbitConnection = _rabbitBusProvider.GetRabbitBus();
-                    await rabbitConnection.PubSub.PublishAsync(submissionCheckRequest,
-                        topic: submission.CheckerSystemName,
-                        cancellationToken: stoppingToken);
-
-                    await dbContext.SaveChangesAsync(stoppingToken);
                 }
-
-                await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+                }
             }
         }
     }
