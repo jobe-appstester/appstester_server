4ec296c3329efd3368726bc5af6ee6701f154705
Maksim Makushchenko <maksalmak@gmail.com>
2022-12-22 11:30:17 +0300

    Added metrics to andrpod checker

--- AppsTester.Checker.Android/Adb/AdbClientProvider.cs
+++ AppsTester.Checker.Android/Adb/AdbClientProvider.cs
@@ -1,5 +1,9 @@
 using System;
+using System.IO;
 using System.Net;
+using System.Runtime.InteropServices;
+using AppsTester.Checker.Android.Metrics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SharpAdbClient;
@@ -15,15 +19,28 @@ namespace AppsTester.Checker.Android.Adb
     {
         private IAdbClient _adbClient;
         private IDeviceMonitor _deviceMonitor;
+
+        private readonly IMetricsService _metricsService;
         private readonly ILogger<AdbClientProvider> _logger;
 
-        public AdbClientProvider(IOptions<AdbOptions> adbOptions, ILogger<AdbClientProvider> logger)
+        public AdbClientProvider(IConfiguration configuration, IOptions<AdbOptions> adbOptions, IMetricsService metricsService, ILogger<AdbClientProvider> logger)
         {
             var dnsEndPoint = new DnsEndPoint(adbOptions.Value.Host, port: 5037);
+            _metricsService = metricsService;
             _logger = logger;
 
+            SetupAdbServer(Path.Combine(configuration.GetValue<string>("ANDROID_SDK_ROOT"), "platform-tools", RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "adb.exe" : "adb"));
             SetupAdbClient(adbOptions.Value, dnsEndPoint);
             SetupDeviceMonitor(dnsEndPoint);
+        }
+        private void SetupAdbServer(string adbPath)
+        {
+            if (!AdbServer.Instance.GetStatus().IsRunning)
+            {
+                _logger.LogInformation("ADB server {adbPath} is not running", adbPath);
+                var startResult = AdbServer.Instance.StartServer(adbPath, false);
+                _logger.LogInformation("ADB server start result {startResult}", startResult);
+            }
         }
         private void SetupAdbClient(AdbOptions configuration, EndPoint dnsEndPoint)
         {
@@ -43,11 +60,22 @@ namespace AppsTester.Checker.Android.Adb
         {
             _deviceMonitor = new DeviceMonitor(new AdbSocket(dnsEndPoint));
 
-            _deviceMonitor.DeviceConnected += (_, args) => _logger.LogInformation("Connected device with serial {Serial}", args.Device.Serial);
+            _deviceMonitor.DeviceConnected += (_, args) =>
+            {
+                _metricsService.CaptureDeviceConnected(args.Device.Serial);
+                _logger.LogInformation("Connected device with serial {Serial}", args.Device.Serial);
+            };
 
-            _deviceMonitor.DeviceDisconnected += (_, args) => _logger.LogInformation("Disconnected device with serial {Serial}", args.Device.Serial);
+            _deviceMonitor.DeviceDisconnected += (_, args) =>
+            {
+                _metricsService.CaptureDeviceDisconnected(args.Device.Serial);
+                _logger.LogInformation("Disconnected device with serial {Serial}", args.Device.Serial);
+            };
 
-            _deviceMonitor.DeviceChanged += (_, args) => _logger.LogInformation("Changed device with serial {Serial}", args.Device.Serial);
+            _deviceMonitor.DeviceChanged += (_, args) =>
+            {
+                _logger.LogInformation("Changed device with serial {Serial}", args.Device.Serial);
+            };
 
             _deviceMonitor.Start();
         }
--- README.md
+++ README.md
@@ -23,3 +23,51 @@ sequenceDiagram
     Controller ->> Moodle: Обновление результата решения
 
 ```
+
+
+## Вспомогательные сервисы
+Для работы в режиме разработки вспомогательные сервисы наряду с разрабатываемыми представлены в `docker-compose.yml`. При запуске они хранят данные под папкой `dev-data` в корне проекта.
+### RabbitMQ
+Если в системе не установлен RabbitMQ, для разработки можно поднять его экземпляр с пользователем `root:root` командой
+```
+docker compose up -d rabbitmq
+```
+После этого сам RabbitMQ будет доступен на порту `5672`, строка подключения будет `amqp://root:root@localhost:5672`
+
+Web интерфейс будет доступен по адресу [http://localhost:15672](http://localhost:15672)
+
+### Сервисы работы с метриками
+Для работы с метриками используются [OpenTelemetry Collector](https://opentelemetry.io/docs/collector/), [Prometheus](https://prometheus.io/) и [Grafana](https://grafana.com). Если они не установлены в системе, для разработки мх можно поднять командой
+```
+docker compose up -d otel-collector prometheus grafana
+```
+* OpenTelemetry Collector конфигурация представлена в `otel-collector-config.yaml`, он занимает несколько портов
+    * `4317` для отправки на него метрик по gRPC со стороны сервисов. Строка подключения в конфигурации -  `"OtlpExporterEndpoint": "http://localhost:4317"`
+    * `8888` по пути `/metrics` будут раздаваться метрики самого коллектора
+    * `8889` по пути `/metrics` будут раздаваться метрики сервисов, которые прислали метрики
+* Prometheus конфигурация представлена в `prometheus.yaml`, он занимает порт `9090`
+* Grafana не имеет изначальной конфигурации, занимает порт `3000`
+
+## Метрики
+```mermaid
+sequenceDiagram
+    participant Controller
+    participant Checker.Android
+    participant otel_collector
+    participant Prometheus
+    participant Grafana
+    link otel_collector: Docs @ https://opentelemetry.io/docs/collector/
+    link Prometheus: Docs @ https://prometheus.io/
+    link Grafana: Docs @ https://grafana.com
+    loop отправка метрик
+        Controller ->> otel_collector: gRPC
+        Checker.Android ->> otel_collector: gRPC
+    end
+    Note right of Checker.Android: .NET шлет данные в формате OpenTelemetry
+    loop стягивание метрик
+      Prometheus ->> otel_collector: GET /metrics
+    end
+    loop обновление dashboard
+      Grafana ->> Prometheus: 
+    end
+```
